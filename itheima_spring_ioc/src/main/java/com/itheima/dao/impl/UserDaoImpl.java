4f6d3098889c96bd2d361598d58119001583768b
ghostchan <jinnchan@qq.com>
2021-10-18 21:27:08 +0800

    08-Spring配置文件-详解2

--- itheima_spring_ioc/src/main/java/com/itheima/dao/impl/UserDaoImpl.java
+++ itheima_spring_ioc/src/main/java/com/itheima/dao/impl/UserDaoImpl.java
@@ -3,6 +3,11 @@ package com.itheima.dao.impl;
 import com.itheima.dao.UserDao;
 
 public class UserDaoImpl implements UserDao {
+
+    public UserDaoImpl() {
+        System.out.println("UserDaoImpl创建...");
+    }
+
     @Override
     public void save() {
         System.out.println("save running...");

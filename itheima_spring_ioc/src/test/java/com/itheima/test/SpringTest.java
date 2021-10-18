1f0f0aefd1209a01c6290a9ef0b2b3d3af1cc2e4
ghostchan <jinnchan@qq.com>
2021-10-18 21:45:18 +0800

    09-Spring配置文件-详解3

--- itheima_spring_ioc/src/test/java/com/itheima/test/SpringTest.java
+++ itheima_spring_ioc/src/test/java/com/itheima/test/SpringTest.java
@@ -9,10 +9,11 @@ public class SpringTest {
     @Test
     //测试scope属性
     public void test1() {
-        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ClassPathXmlApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
         UserDao userDao1 = (UserDao) app.getBean("userDao");
-        UserDao userDao2 = (UserDao) app.getBean("userDao");
+//        UserDao userDao2 = (UserDao) app.getBean("userDao");
         System.out.println(userDao1);
-        System.out.println(userDao2);
+//        System.out.println(userDao2);
+        app.close();
     }
 }

8cea6b5b6606dbd41646272e100f2f14cfcd8ab3
ghostchan <jinnchan@qq.com>
2021-10-19 19:04:37 +0800

    16-Spring配置文件-依赖注入6

--- itheima_spring_ioc/src/main/java/com/itheima/dao/impl/UserDaoImpl.java
+++ itheima_spring_ioc/src/main/java/com/itheima/dao/impl/UserDaoImpl.java
@@ -4,20 +4,32 @@ import com.itheima.dao.UserDao;
 
 public class UserDaoImpl implements UserDao {
 
-    public UserDaoImpl() {
-        System.out.println("UserDaoImpl创建...");
+    private String username;
+    private int age;
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
-    public void init() {
-        System.out.println("初始化方法...");
+    public void setAge(int age) {
+        this.age = age;
     }
 
-    public void destory() {
-        System.out.println("销毁方法...");
-    }
+    //    public UserDaoImpl() {
+//        System.out.println("UserDaoImpl创建...");
+//    }
+//
+//    public void init() {
+//        System.out.println("初始化方法...");
+//    }
+//
+//    public void destory() {
+//        System.out.println("销毁方法...");
+//    }
 
     @Override
     public void save() {
+        System.out.println(username + "====" + age);
         System.out.println("save running...");
     }
 }

965663ad08ee64f436148994294c2d477caf1a47
ghostchan <jinnchan@qq.com>
2021-10-19 20:17:05 +0800

    17-Spring配置文件-依赖注入7

--- itheima_spring_ioc/src/main/java/com/itheima/dao/impl/UserDaoImpl.java
+++ itheima_spring_ioc/src/main/java/com/itheima/dao/impl/UserDaoImpl.java
@@ -1,8 +1,29 @@
 package com.itheima.dao.impl;
 
 import com.itheima.dao.UserDao;
+import com.itheima.domain.User;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 
 public class UserDaoImpl implements UserDao {
+
+    private List<String> strList;
+    private Map<String, User> userMap;
+    private Properties properties;
+
+    public void setStrList(List<String> strList) {
+        this.strList = strList;
+    }
+
+    public void setUserMap(Map<String, User> userMap) {
+        this.userMap = userMap;
+    }
+
+    public void setProperties(Properties properties) {
+        this.properties = properties;
+    }
 
     private String username;
     private int age;
@@ -29,7 +50,10 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public void save() {
-        System.out.println(username + "====" + age);
+//        System.out.println(username + "====" + age);
+        System.out.println(strList);
+        System.out.println(userMap);
+        System.out.println(properties);
         System.out.println("save running...");
     }
 }

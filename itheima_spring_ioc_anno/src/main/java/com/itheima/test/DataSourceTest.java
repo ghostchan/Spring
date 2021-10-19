032f89637afa963ff5eeec251bb9cb17b94afeae
ghostchan <jinnchan@qq.com>
2021-10-19 22:31:53 +0800

    04-Spring配置数据源-手动创建Druid数据源

--- itheima_spring_ioc_anno/src/main/java/com/itheima/test/DataSourceTest.java
+++ itheima_spring_ioc_anno/src/main/java/com/itheima/test/DataSourceTest.java
@@ -1,12 +1,25 @@
 package com.itheima.test;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.junit.Test;
 
-import java.beans.PropertyVetoException;
 import java.sql.Connection;
 
 public class DataSourceTest {
+
+    @Test
+    //测试手动创建druid数据源
+    public void test2() throws Exception {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/mhl");
+        dataSource.setUsername("root");
+        dataSource.setPassword("root-123456");
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+        connection.close();
+    }
 
     @Test
     //测试手动创建c3p0数据源

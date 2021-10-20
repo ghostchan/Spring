267b5e914c84184979e3ae4440589755cf44e159
ghostchan <jinnchan@qq.com>
2021-10-20 20:26:21 +0800

    10-Spring注解开发-原始注解入门操作

--- itheima_spring_ioc_anno/src/main/java/com/itheima/dao/impl/UserDaoImpl.java
+++ itheima_spring_ioc_anno/src/main/java/com/itheima/dao/impl/UserDaoImpl.java
@@ -1,7 +1,10 @@
 package com.itheima.dao.impl;
 
 import com.itheima.dao.UserDao;
+import org.springframework.stereotype.Component;
 
+//<bean id="userDao" class="com.itheima.dao.impl.UserDaoImpl"></bean>
+@Component("userDao")
 public class UserDaoImpl implements UserDao {
     @Override
     public void save() {
--- itheima_spring_ioc_anno/src/main/java/com/itheima/service/impl/UserServiceImpl.java
+++ itheima_spring_ioc_anno/src/main/java/com/itheima/service/impl/UserServiceImpl.java
@@ -2,8 +2,17 @@ package com.itheima.service.impl;
 
 import com.itheima.dao.UserDao;
 import com.itheima.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+//<bean id="userService" class="com.itheima.service.impl.UserServiceImpl">
+@Component("userService")
 public class UserServiceImpl implements UserService {
+
+    //    <property name="userDao" ref="userDao"></property>
+    @Autowired
+    @Qualifier("userDao")
     private UserDao userDao;
 
     public void setUserDao(UserDao userDao) {

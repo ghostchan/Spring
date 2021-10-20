4d5303c6593d059e5b3ffe74ac83ce15e2b30cee
ghostchan <jinnchan@qq.com>
2021-10-20 21:37:56 +0800

    12-Spring注解开发-原始注解详解2

--- itheima_spring_ioc_anno/src/main/java/com/itheima/service/impl/UserServiceImpl.java
+++ itheima_spring_ioc_anno/src/main/java/com/itheima/service/impl/UserServiceImpl.java
@@ -4,15 +4,24 @@ import com.itheima.dao.UserDao;
 import com.itheima.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.annotation.Resource;
 
 //<bean id="userService" class="com.itheima.service.impl.UserServiceImpl">
 //@Component("userService")
 @Service("userService")
+//@Scope("prototype")
+//@Scope("singleton")
 public class UserServiceImpl implements UserService {
+
+    @Value("${jdbc.driver}")
+    private String driver;
 
     //    <property name="userDao" ref="userDao"></property>
 //    @Autowired  //按照数据类型从Spring容器中进行匹配的
@@ -26,6 +35,17 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void save() {
+        System.out.println(driver);
         userDao.save();
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Service对象的初始化方法");
+    }
+
+    @PreDestroy
+    public void destory() {
+        System.out.println("Service对象的销毁方法");
     }
 }
--- itheima_spring_ioc_anno/src/main/java/com/itheima/web/UserController.java
+++ itheima_spring_ioc_anno/src/main/java/com/itheima/web/UserController.java
@@ -6,8 +6,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class UserController {
     public static void main(String[] args) {
-        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ClassPathXmlApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
         UserService userService = app.getBean(UserService.class);
         userService.save();
+        app.close();
     }
 }

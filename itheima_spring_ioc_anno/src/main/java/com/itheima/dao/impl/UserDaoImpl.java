acba14400cd32e59c1bc5f4b90b3ab2f4e205247
ghostchan <jinnchan@qq.com>
2021-10-20 20:49:01 +0800

    11-Spring注解开发-原始注解详解1

--- itheima_spring_ioc_anno/src/main/java/com/itheima/dao/impl/UserDaoImpl.java
+++ itheima_spring_ioc_anno/src/main/java/com/itheima/dao/impl/UserDaoImpl.java
@@ -2,9 +2,11 @@ package com.itheima.dao.impl;
 
 import com.itheima.dao.UserDao;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 //<bean id="userDao" class="com.itheima.dao.impl.UserDaoImpl"></bean>
-@Component("userDao")
+//@Component("userDao")
+@Repository("userDao")
 public class UserDaoImpl implements UserDao {
     @Override
     public void save() {
--- itheima_spring_ioc_anno/src/main/java/com/itheima/service/impl/UserServiceImpl.java
+++ itheima_spring_ioc_anno/src/main/java/com/itheima/service/impl/UserServiceImpl.java
@@ -5,19 +5,24 @@ import com.itheima.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 //<bean id="userService" class="com.itheima.service.impl.UserServiceImpl">
-@Component("userService")
+//@Component("userService")
+@Service("userService")
 public class UserServiceImpl implements UserService {
 
     //    <property name="userDao" ref="userDao"></property>
-    @Autowired
-    @Qualifier("userDao")
+//    @Autowired  //按照数据类型从Spring容器中进行匹配的
+//    @Qualifier("userDao")  //是按照id值从容器中进行匹配的 但是主要此处@Qualifier结合@Autowired一起使用
+    @Resource(name = "userDao") //@Resource相当于 @Autowired  + @Qualifier
     private UserDao userDao;
 
-    public void setUserDao(UserDao userDao) {
-        this.userDao = userDao;
-    }
+//    public void setUserDao(UserDao userDao) {
+//        this.userDao = userDao;
+//    }
 
     @Override
     public void save() {

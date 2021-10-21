0cf8f3f5049e786f519fe52bbef6b50b9c6e65a5
ghostchan <jinnchan@qq.com>
2021-10-21 23:01:29 +0800

    11-xml方式实现aop-切点表达式的抽取

--- itheima_spring_aop/src/main/java/com/itheima/aop/Target.java
+++ itheima_spring_aop/src/main/java/com/itheima/aop/Target.java
@@ -8,6 +8,6 @@ public class Target implements TargetInterface {
     public void save() {
 
         System.out.println("save running.....");
-        int i = 1 / 0;
+//        int i = 1 / 0;
     }
 }

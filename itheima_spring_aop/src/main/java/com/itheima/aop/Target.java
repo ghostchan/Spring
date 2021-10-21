5cd5d59c2fd85ef5a54d41d13629a5ca1ad8ca24
ghostchan <jinnchan@qq.com>
2021-10-21 22:40:11 +0800

    10-xml方式实现aop-通知的种类

--- itheima_spring_aop/src/main/java/com/itheima/aop/Target.java
+++ itheima_spring_aop/src/main/java/com/itheima/aop/Target.java
@@ -6,6 +6,8 @@ package com.itheima.aop;
 public class Target implements TargetInterface {
     @Override
     public void save() {
+
         System.out.println("save running.....");
+        int i = 1 / 0;
     }
 }

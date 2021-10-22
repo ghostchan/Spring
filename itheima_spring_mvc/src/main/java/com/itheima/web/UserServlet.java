e0e098746fee2b49cd89124b8bbde95b427d42e3
ghostchan <jinnchan@qq.com>
2021-10-22 20:43:34 +0800

    03-Spring集成web环境-自定义ContextLoaderListener1

--- itheima_spring_mvc/src/main/java/com/itheima/web/UserServlet.java
+++ itheima_spring_mvc/src/main/java/com/itheima/web/UserServlet.java
@@ -4,6 +4,7 @@ import com.itheima.service.UserService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,7 +15,10 @@ public class UserServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+//        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+//        req.getServletContext();
+        ServletContext servletContext = this.getServletContext();
+        ApplicationContext app = (ApplicationContext) servletContext.getAttribute("app");
         UserService userService = app.getBean(UserService.class);
         userService.save();
         System.out.println("test");

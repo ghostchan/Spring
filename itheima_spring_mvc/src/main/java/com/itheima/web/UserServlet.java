8091d7eea328e161714f792065e92b4672125615
ghostchan <jinnchan@qq.com>
2021-10-22 21:46:14 +0800

    05-Spring集成web环境-Spring集成web环境代码实现

--- itheima_spring_mvc/src/main/java/com/itheima/web/UserServlet.java
+++ itheima_spring_mvc/src/main/java/com/itheima/web/UserServlet.java
@@ -1,8 +1,9 @@
 package com.itheima.web;
 
-import com.itheima.listener.WebApplicationContextUtils;
 import com.itheima.service.UserService;
 import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -19,6 +20,7 @@ public class UserServlet extends HttpServlet {
 //        req.getServletContext();
         ServletContext servletContext = this.getServletContext();
 //        ApplicationContext app = (ApplicationContext) servletContext.getAttribute("app");
+//        ApplicationContext app = WebApplicationContextUtils.getWebApplicationContext(servletContext);
         ApplicationContext app = WebApplicationContextUtils.getWebApplicationContext(servletContext);
         UserService userService = app.getBean(UserService.class);
         userService.save();

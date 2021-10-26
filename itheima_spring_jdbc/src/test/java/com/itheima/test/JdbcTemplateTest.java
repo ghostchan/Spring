0b8d330b2f07a452a658857405976976a22de6f1
ghostchan <jinnchan@qq.com>
2021-10-26 20:27:10 +0800

    05-JdbcTemplate基本使用-spring产生模板对象代码实现

--- itheima_spring_jdbc/src/test/java/com/itheima/test/JdbcTemplateTest.java
+++ itheima_spring_jdbc/src/test/java/com/itheima/test/JdbcTemplateTest.java
@@ -2,11 +2,22 @@ package com.itheima.test;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.beans.PropertyVetoException;
 
 public class JdbcTemplateTest {
+
+    @Test
+    //测试jdbcTemplate对象
+    public void test2() {
+        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+        JdbcTemplate jdbcTemplate = app.getBean(JdbcTemplate.class);
+        int row = jdbcTemplate.update("insert into account values(?,?)", "zhangsan", 5000);
+        System.out.println(row);
+    }
 
     @Test
     //测试JdbcTemplate开发步骤
@@ -22,7 +33,7 @@ public class JdbcTemplateTest {
         //设置数据源对象 知道数据库在哪
         jdbcTemplate.setDataSource(dataSource);
         //执行操作
-        int row = jdbcTemplate.update("insert into account values(?,?)", "tom", 5000);
+        int row = jdbcTemplate.update("insert into account values(?,?)", "jack", 6000);
         System.out.println(row);
     }
 }

558255a652d50208b28d13a8db0d73ffc7a09d71
ghostchan <jinnchan@qq.com>
2021-10-29 23:02:33 +0800

    10-Spring练习-角色列表展示-配置实现

--- itheima_spring_test/src/main/java/com/itheima/controller/RoleController.java
+++ itheima_spring_test/src/main/java/com/itheima/controller/RoleController.java
@@ -2,19 +2,19 @@ package com.itheima.controller;
 
 import com.itheima.domain.Role;
 import com.itheima.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
 @RequestMapping("/role")
+@Controller
 public class RoleController {
 
+    @Autowired
     private RoleService roleService;
-
-    public void setRoleService(RoleService roleService) {
-        this.roleService = roleService;
-    }
 
     @RequestMapping("/list")
     public ModelAndView list() {

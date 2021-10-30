10879c7d42cbb26464c13194b0e93bb64151a374
ghostchan <jinnchan@qq.com>
2021-10-30 09:48:32 +0800

    13-Spring练习-用户列表展示1

---
+++ itheima_spring_test/src/main/java/com/itheima/service/impl/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.itheima.service.impl;
+
+import com.itheima.dao.UserDao;
+import com.itheima.domain.User;
+import com.itheima.service.UserService;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+
+    private UserDao userDao;
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    public List<User> list() {
+        return userDao.findAll();
+    }
+}

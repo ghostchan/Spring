6fac795eb9fdc1a3683c91ea91919569ff2c5a9a
ghostchan <jinnchan@qq.com>
2021-11-01 23:52:03 +0800

    08-mybatis的增删改查-修改和删除操作

--- itheima_mybatis_quick/src/test/java/com/itheima/test/MyBatisTest.java
+++ itheima_mybatis_quick/src/test/java/com/itheima/test/MyBatisTest.java
@@ -14,6 +14,26 @@ import java.util.List;
 public class MyBatisTest {
 
     @Test
+    //删除操作
+    public void test4() throws IOException {
+
+        //获得核心配置文件
+        InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+        //获得session工厂对象
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        //获得session会话对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        //执行操作 参数: namespace+id
+        sqlSession.update("userMapper.delete", 5);
+
+        //mybatis执行更新操作 提交事务
+        sqlSession.commit();
+        //释放资源
+        sqlSession.close();
+
+    }
+
+    @Test
     //更新操作
     public void test3() throws IOException {
 

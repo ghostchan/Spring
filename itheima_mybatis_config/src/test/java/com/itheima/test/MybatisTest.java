cb097217a6edb04754ef594081e9f3b2325524e7
ghostchan <jinnchan@qq.com>
2021-11-06 19:42:26 +0800

    11-mybatis核心配置文件深入-typeHandler测试环境搭建

---
+++ itheima_mybatis_config/src/test/java/com/itheima/test/MybatisTest.java
@@ -0,0 +1,40 @@
+package com.itheima.test;
+
+import com.itheima.domain.User;
+import com.itheima.mapper.UserMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+
+public class MybatisTest {
+
+    @Test
+    public void test1() throws IOException {
+        InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+
+        //创建user
+        User user = new User();
+        user.setUsername("ceshi");
+        user.setPassword("abc");
+        user.setBirthday(new Date());
+
+        //执行保存操作
+        mapper.save(user);
+
+        sqlSession.commit();
+        sqlSession.close();
+
+
+    }
+
+}

8a10b3d7f1397ea584c8b7fbb43c5dd45c06ab9b
ghostchan <jinnchan@qq.com>
2021-11-06 16:05:34 +0800

    05-mybatis映射文件深入-动态sql概述和环境搭建

---
+++ itheima_mybatis_mapper/src/test/java/com/itheima/test/MapperTest.java
@@ -0,0 +1,36 @@
+package com.itheima.test;
+
+import com.itheima.domain.User;
+import com.itheima.mapper.UserMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class MapperTest {
+
+    @Test
+    public void test1() throws IOException {
+        InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+
+        //模拟条件
+        User condition = new User();
+        condition.setId(1);
+        condition.setUsername("zhangsan");
+        condition.setPassword("123");
+
+        List<User> userList = mapper.findByCondition(condition);
+
+        System.out.println(userList);
+    }
+
+}

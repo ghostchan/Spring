ec54ea5d492ba66035524bd027c4e22eed197f68
ghostchan <jinnchan@qq.com>
2021-11-07 13:10:19 +0800

    10-mybatis注解开发-注解完成基本的crud

--- itheima_mybatis_anno/src/main/java/com/itheima/mapper/UserMapper.java
+++ itheima_mybatis_anno/src/main/java/com/itheima/mapper/UserMapper.java
@@ -1,18 +1,27 @@
 package com.itheima.mapper;
 
 import com.itheima.domain.User;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
 public interface UserMapper {
 
+    @Insert("insert into user values (#{id}, #{username}, #{password}, #{birthday})")
     public void save(User user);
 
+    @Update("update user set username=#{username},password=#{password} where id = #{id}")
     public void update(User user);
 
+    @Delete("delete from user where id = #{id}")
     public void delete(int id);
 
+    @Select("select * from user where id = #{id}")
     public User findById(int id);
 
+    @Select("select * from user")
     public List<User> findAll();
 }
